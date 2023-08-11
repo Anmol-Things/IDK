6d3c29ae4a4eea1f7faa02d1e74cead116693db6
Anmol-Things <141685346+Anmol-Things@users.noreply.github.com>
2023-08-11 17:48:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,125 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+// $(document).ready(function(){
+//     function takeToQuickServe() {
+//         window.open("https://quickserve.jazcodeit.me/", "_blank");
+//     }
+
+//     $(window).scroll(function(){
+//         // sticky navbar on scroll script
+//         if(this.scrollY > 20){
+//             $('.navbar').addClass("sticky");
+//         }else{
+//             $('.navbar').removeClass("sticky");
+//         }
+        
+//         // scroll-up button show/hide script
+//         if(this.scrollY > 500){
+//             $('.scroll-up-btn').addClass("show");
+//         }else{
+//             $('.scroll-up-btn').removeClass("show");
+//         }
+//     });
+
+//     // slide-up script
+//     $('.quickserve').click(function() {
+//         takeToQuickServe();
+//     });
+
+//     $('.scroll-up-btn').click(function(){
+//         $('html').animate({scrollTop: 0});
+//         // removing smooth scroll on slide-up button click
+//         $('html').css("scrollBehavior", "auto");
+//     });
+
+//     $('.navbar .menu li a').click(function(){
+//         // applying again smooth scroll on menu items click
+//         $('html').css("scrollBehavior", "smooth");
+//     });
+
+//     // toggle menu/navbar script
+//     $('.menu-btn').click(function(){
+//         $('.navbar .menu').toggleClass("active");
+//         $('.menu-btn i').toggleClass("active");
+//     });
+
+//     // typing text animation script
+//     var typed = new Typed(".typing", {
+//         strings: ["Software Developer", "Full-Stack Developer", "Web Developer", "Programmer", "Freelancer"],
+//         typeSpeed: 100,
+//         backSpeed: 60,
+//         loop: true
+//     });
+
+//     var typed = new Typed(".typing-2", {
+//         strings: ["Software Developer", "Web Developer", "Programmer"],
+//         typeSpeed: 100,
+//         backSpeed: 60,
+//         loop: true
+//     });
+
+//     // owl carousel script
+//     $('.carousel').owlCarousel({
+//         margin: 20,
+//         loop: true,
+//         autoplay: true,
+//         autoplayTimeOut: 2000,
+//         autoplayHoverPause: true,
+//         responsive: {
+//             0:{
+//                 items: 1,
+//                 nav: false
+//             },
+//             600:{
+//                 items: 2,
+//                 nav: false
+//             },
+//             1000:{
+//                 items: 3,
+//                 nav: false
+//             }
+//         }
+//     });
+// });
+
+
+$(document).ready(function() {
+    function takeToQuickServe() {
+        window.open("https://quickserve.jazcodeit.me/", "_blank");
+    }
+
+    $(window).scroll(function() {
         // sticky navbar on scroll script
-        if(this.scrollY > 20){
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-        
+
         // scroll-up button show/hide script
-        if(this.scrollY > 500){
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
 
     // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+    $('.quickserve').click(function() {
+        takeToQuickServe();
+    });
+
+    $('.scroll-up-btn').click(function() {
+        $('html').animate({ scrollTop: 0 });
         // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
 
-    $('.navbar .menu li a').click(function(){
+    $('.navbar .menu li a').click(function() {
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
     // toggle menu/navbar script
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function() {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
@@ -56,18 +147,48 @@ $(document).ready(function(){
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
                 nav: false
             },
-            600:{
+            600: {
                 items: 2,
                 nav: false
             },
-            1000:{
+            1000: {
                 items: 3,
                 nav: false
             }
         }
     });
+
+// Form Submission with SweetAlert notification
+$('form').submit(function(event) {
+    event.preventDefault();
+
+    const form = this;
+    const formData = new FormData(form);
+
+    Swal.fire({
+        icon: 'success',
+        title: 'Message Sent!',
+        text: 'I will get back to you as soon as I can.',
+    }).then(function() {
+        // Submit the form using AJAX
+        $.ajax({
+            type: 'POST',
+            url: form.action, // Use the form's action attribute
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function(response) {
+                console.log('Form submitted successfully:', response);
+            },
+            error: function(error) {
+                console.error('Error submitting form:', error);
+            }
+        });
+    });
+});
+
 });
